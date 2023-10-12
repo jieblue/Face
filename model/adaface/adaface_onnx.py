e753f00b4f2006c7a478bbeb6f44e2ef4c110476
jieblue <2775856803@qq.com>
2023-10-12 11:04:48 +0800

    新增model包的方法注释说明

--- model/adaface/adaface_onnx.py
+++ model/adaface/adaface_onnx.py
@@ -2,8 +2,11 @@ import onnxruntime as ort
 import numpy as np
 
 
+# adaface 的 ONNX模型使用
+# 把人脸图片转为特征向量
 class Adaface_Onnx:
     def __init__(self, path, cuda=True):
+        #使用GPU
         if cuda:
             providers = ['CUDAExecutionProvider', 'CPUExecutionProvider']
             self.device = 'cuda'
@@ -11,12 +14,16 @@ class Adaface_Onnx:
             providers = ['CPUExecutionProvider']
             self.device = 'cpu'
 
+
         so = ort.SessionOptions()
         so.log_severity_level = 3
+
+        # 加载 onnx模型到onnxruntime的推理
         self.session = ort.InferenceSession(path, so, providers=providers)
+        # onnx获取输入名称
         self.input_name = self.session.get_inputs()[0].name
 
-
+    # 获得模型的输出
     def forward(self, img):
         input_img = to_input(img)
         inputs = {self.input_name: input_img}
@@ -28,7 +35,8 @@ class Adaface_Onnx:
         return embeddings[0][0]
 
 
-
+# 转为adace onnx 的输入
+# img 图片格式为 [ h, w, c] bgr 0-255整数
 def to_input(img):
     # cv2 bgr img 0-255
     img = ((img / 255.) - 0.5) / 0.5
--- model/tface/tface_onnx.py
+++ model/tface/tface_onnx.py
@@ -8,7 +8,40 @@ import numpy as np
 
 # from utils.face_helper import pre_process, post_process
 
+# TFace的ONNX模型， 用于得出质量分数
+class TFace_Onnx:
+    def __init__(self, path, cuda=True):
+        #使用GPU
+        if cuda:
+            providers = ['CUDAExecutionProvider', 'CPUExecutionProvider']
+            self.device = 'cuda'
+        else:
+            providers = ['CPUExecutionProvider']
+            self.device = 'cpu'
 
+        so = ort.SessionOptions()
+        so.log_severity_level = 3
+        # 加载onnx模型到onnxruntime的推理
+        self.session = ort.InferenceSession(path, so, providers=providers)
+        self.input_name = self.session.get_inputs()[0].name
+        #模型预热
+        self.warmup()
+
+    # 得到模型输出
+    def forward(self, img):
+        img = to_input(img)
+        inputs = {self.input_name: img}
+        res = self.session.run(None, inputs)[0][0][0]
+        return res
+
+
+    # 模型预热
+    def warmup(self):
+        x = numpy.random.random([112, 112, 3])
+        self.forward(x)
+
+
+# 转为模型输入要求
 def to_input(img):
     # img is a bgr numpy array range(0, 255)
     img = img / 255.0
@@ -20,32 +53,3 @@ def to_input(img):
     img = np.float32(img[np.newaxis, :, :, :])
     img = img.transpose(0, 3, 1, 2)
     return img
-
-
-class TFace_Onnx:
-    def __init__(self, path, cuda=True):
-        if cuda:
-            providers = ['CUDAExecutionProvider', 'CPUExecutionProvider']
-            self.device = 'cuda'
-        else:
-            providers = ['CPUExecutionProvider']
-            self.device = 'cpu'
-
-        so = ort.SessionOptions()
-        so.log_severity_level = 3
-        self.session = ort.InferenceSession(path, so, providers=providers)
-        self.input_name = self.session.get_inputs()[0].name
-        self.warmup()
-
-
-    def forward(self, img):
-        img = to_input(img)
-        inputs = {self.input_name: img}
-        res = self.session.run(None, inputs)[0][0][0]
-        return res
-
-
-    def warmup(self):
-        x = numpy.random.random([112, 112, 3])
-        self.forward(x)
-
