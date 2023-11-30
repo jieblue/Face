ba2b8c681aaf424d4196fffcdc0fbf51fd5700a5
JackPan <826647196@qq.com>
2023-11-30 19:54:32 +0800

    v3 process_video_file

--- entity/frame_entity.py
+++ entity/frame_entity.py
@@ -23,31 +23,47 @@ class KeyFrame:
         self.frame = frame
         self.video_id = video_id
         # TODO 增加关键帧在HDFS中所在位置
-        self.hdfs_path = None
+        self.hdfs_path = hdfs_prefix + "/key_frame/" + self.file_name + "/" + str(self.frame_num) + ".jpg"
         # 关键帧的唯一ID
         self.key_id = self.generate_key_id()
 
     def generate_key_id(self):
         return self.file_name + "_" + str(self.frame_num) + "_" + str(self.timestamp)
 
+    def to_dict(self):
+        return {
+            "file_name": self.file_name,
+            "frame_num": self.frame_num,
+            "timestamp": self.timestamp,
+            "video_id": self.video_id,
+            "hdfs_path": self.hdfs_path,
+            "key_id": self.key_id
+        }
+
 
 class FaceKeyFrame:
-    def __init__(self, file_name, video_id, frame_num, timestamp, face_num, face_frame):
+    def __init__(self, *, file_name, video_id, frame_num, timestamp, face_num, face_frame):
         self.file_name = file_name
         self.frame_num = frame_num
         self.timestamp = timestamp
         self.video_id = video_id
         self.face_num = face_num
         self.face_frame = face_frame
-        # TODO 增加人脸在HDFS中所在位置
-        self.hdfs_path = None
-
         # TODO 增加人脸在此关键帧图片中所在位置
         # self.face_location = face_location
         self.key_id = self.generate_key_id()
+        self.hdfs_path = hdfs_prefix + "/face/" + self.file_name + "/" + str(self.key_id) + ".jpg"
 
     def generate_key_id(self):
         return self.file_name + "_" + str(self.frame_num) + "_" + str(self.timestamp) + "_" + str(self.face_num)
 
-    def generate_hdfs_path(self):
-        return hdfs_prefix + "/" + self.file_name + "/" + str(self.key_id) + ".jpg"
+    def to_dict(self):
+        return {
+            "file_name": self.file_name,
+            "frame_num": self.frame_num,
+            "timestamp": self.timestamp,
+            "video_id": self.video_id,
+            "face_num": self.face_num,
+            "hdfs_path": self.hdfs_path,
+            "key_id": self.key_id
+        }
