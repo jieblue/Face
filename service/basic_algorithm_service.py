9b6fa768fe42ff478b46b2f8994613960693938f
jieblue <2775856803@qq.com>
2024-03-04 10:14:18 +0800

    更新提取关键帧代码

--- service/basic_algorithm_service.py
+++ service/basic_algorithm_service.py
@@ -17,52 +17,51 @@ logger = log_util.get_logger(__name__)
 
 
 def extract_key_frame_list(video_file: VideoFile) -> List[KeyFrame]:
-    container = av.open(video_file.file_path)
+    content = av.datasets.curated(video_file.file_path)
+    with av.open(content) as container:
 
-    result = []
-    # Signal that we only want to look at keyframes.
-    stream = container.streams.video[0]
+        result = []
+        # Signal that we only want to look at keyframes.
+        stream = container.streams.video[0]
 
-    stream.codec_context.skip_frame = 'NONINTRA'
-    frames = container.decode(stream)
-    frame_num = 1
-    # 关键帧提取
-    for frame in frames:
-        timestamp = round(frame.pts * stream.time_base)
-        np_frame = av_frame2np(frame)
-        key_frame = KeyFrame(file_name=video_file.file_name, video_id=video_file.video_id, frame_num=frame_num,
-                             timestamp=timestamp, frame=np_frame, frame_stream=frame, tag=video_file.tag)
-        result.append(key_frame)
-        frame_num = frame_num + 1
+        stream.codec_context.skip_frame = 'NONINTRA'
+        frames = container.decode(stream)
+        frame_num = 1
+        # 关键帧提取
+        for frame in frames:
+            timestamp = round(frame.pts * stream.time_base)
+            np_frame = av_frame2np(frame)
+            key_frame = KeyFrame(file_name=video_file.file_name, video_id=video_file.video_id, frame_num=frame_num,
+                                 timestamp=timestamp, frame=np_frame, frame_stream=frame, tag=video_file.tag)
+            result.append(key_frame)
+            frame_num = frame_num + 1
 
-    # logger.info(f"Extracted {len(frames)} frames from {video_file.file_name}")
-    # start_pos = 0
-    # end_pos = 0
-    # length = len(frames)
-    # while end_pos < length:
-    #     if start_pos == end_pos:
-    #         end_pos = end_pos + 1
-    #     else:
-    #         start_frame = frames[start_pos]
-    #         end_frame = frames[end_pos]
-    #         if keyframe_similarity(start_frame, end_frame) > 0.9:
-    #             end_pos = end_pos + 1
-    #         else:
-    #             np_frame = av_frame2np(start_frame)
-    #             timestamp = round(start_frame.pts * stream.time_base)
-    #             key_frame = KeyFrame(file_name=video_file.file_name, video_id=video_file.video_id,
-    #                                  frame_num=frame_num,
-    #                                  timestamp=timestamp, frame=np_frame, frame_stream=start_frame,
-    #                                  tag=video_file.tag)
-    #             result.append(key_frame)
-    #             frame_num = frame_num + 1
-    #             start_pos = end_pos
-    #             end_pos = end_pos + 1
-    # logger.info(f"Extracted {len(result)} key frames from {video_file.file_name}")
+        # logger.info(f"Extracted {len(frames)} frames from {video_file.file_name}")
+        # start_pos = 0
+        # end_pos = 0
+        # length = len(frames)
+        # while end_pos < length:
+        #     if start_pos == end_pos:
+        #         end_pos = end_pos + 1
+        #     else:
+        #         start_frame = frames[start_pos]
+        #         end_frame = frames[end_pos]
+        #         if keyframe_similarity(start_frame, end_frame) > 0.9:
+        #             end_pos = end_pos + 1
+        #         else:
+        #             np_frame = av_frame2np(start_frame)
+        #             timestamp = round(start_frame.pts * stream.time_base)
+        #             key_frame = KeyFrame(file_name=video_file.file_name, video_id=video_file.video_id,
+        #                                  frame_num=frame_num,
+        #                                  timestamp=timestamp, frame=np_frame, frame_stream=start_frame,
+        #                                  tag=video_file.tag)
+        #             result.append(key_frame)
+        #             frame_num = frame_num + 1
+        #             start_pos = end_pos
+        #             end_pos = end_pos + 1
+        # logger.info(f"Extracted {len(result)} key frames from {video_file.file_name}")
 
-    #gc.collect()释放frames占用的内存
-    gc.collect()
-    return result
+        return result
 
 
 def keyframe_similarity(frame_x, frame_y):
